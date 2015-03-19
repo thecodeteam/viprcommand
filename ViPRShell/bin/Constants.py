9446ebe629b1e61e8f038ae1582ffbb220f44942
Keerthi Bala <keerthi.bala2@emc.com>
2015-03-19 12:06:56 -0400

    Renaming project

---
+++ ViPRShell/bin/Constants.py
@@ -0,0 +1,8 @@
+ID_KEY = '{id}'
+BULK_KEY = 'bulk'
+RESPONSE_TYPE_KEY = 'accept'
+RESPONSE_TYPE_XML = 'xml'
+RESPONSE_TYPE_JSON = 'json'
+PARAMS_KEY = 'params'
+ACTIONS_KEY = 'actions'
+COOKIE_FILE_NAME = 'cf'
