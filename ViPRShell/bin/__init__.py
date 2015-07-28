aeb2fc476d366001a8d93c0811e6d7b79de193dd
periap <priya.periaswamy@emc.com>
2015-07-28 15:17:53 -0400

    Moved init.py file to a directory above

--- ViPRShell/bin/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-base_dir = os.path.dirname(__file__) or '.'
-print('Base directory:' + base_dir)
-
-# Insert the package_dir_a directory at the front of the path.
-package_dir = os.path.join(base_dir)
-sys.path.insert(0, package_dir)
